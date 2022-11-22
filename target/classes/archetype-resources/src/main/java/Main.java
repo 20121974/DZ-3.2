3872d90faa85ff2042e98a74a9f4c1785f75699c
20121974 <nfi46102@mail.ru>
2022-11-22 20:12:36 +0300

    1 commit

---
+++ target/classes/archetype-resources/src/main/java/Main.java
@@ -0,0 +1,12 @@
+package $pro.sky.java.couers2;
+
+import com.sun.tools.javac.util.StringUtils;
+import sun.jvm.hotspot.utilities.CStringUtilities;
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+        StringUtils.commons-long
+    }
+}
